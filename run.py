b4107f2bc712c2fba745c9a68aeb7d158f301d45
Blurfnes <98904442+Blurfnes@users.noreply.github.com>
2022-07-04 14:29:31 +0200

    Add files via upload

---
+++ run.py
@@ -0,0 +1,7 @@
+print("Enter the first digit")
+x = input()
+print("Enter the second digit")
+y = input()
+if x > y: print(x, "is bigger then", y)
+if y > x: print(y, "is bigger then", x)
+if y == x: print(y, "and", x, "are equal")
